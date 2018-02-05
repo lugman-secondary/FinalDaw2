7b82662011e50d38e73ba0f1eadcdc955220f8b6
Lugman <lugmansito@gmail.com>
2018-02-05 22:58:47 +0100

    Casi conseguido traer imagenes

---
+++ app/src/main/java/es/com/lugman/appfinal2/Adaptador.java
@@ -0,0 +1,95 @@
+package es.com.lugman.appfinal2;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.drawable.Drawable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.ArrayList;
+
+/**
+ * Created by Lugman on 02/02/2018.
+ */
+
+public class Adaptador extends BaseAdapter {
+    ArrayList<Monedas>list;
+    Context context;
+
+    public Adaptador(ArrayList<Monedas> list, Context context) {
+        this.list = list;
+        this.context = context;
+    }
+
+    @Override
+    public int getCount() {
+        return list.size();
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return list.get(position);
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        ViewHolder viewH;
+        if (convertView == null){
+            viewH = new ViewHolder();
+            LayoutInflater inf = LayoutInflater.from(context);
+            convertView = inf.inflate(R.layout.item,null);
+            viewH.imagen = convertView.findViewById(R.id.imageView);
+            viewH.Nombre = convertView.findViewById(R.id.textView);
+            viewH.Valor = convertView.findViewById(R.id.textView2);
+            viewH.Volumen = convertView.findViewById(R.id.textView3);
+            convertView.setTag(viewH);
+
+        }else {
+            viewH = (ViewHolder) convertView.getTag();
+        }
+        URL imageUrl = null;
+        HttpURLConnection conn = null;
+
+        try {
+
+            imageUrl = new URL("https://img.europapress.es/fotoweb/fotonoticia_20171203143150_640.jpg");
+            conn = (HttpURLConnection) imageUrl.getContent();
+            conn.connect();
+//            Bitmap imagen = BitmapFactory.decodeStream(conn.getInputStream());
+//                   viewH.imagen.setImageBitmap(imagen);
+
+        } catch (IOException e) {
+
+            e.printStackTrace();
+
+        }
+//        viewH.imagen.setImageDrawable(d);
+        viewH.Nombre.setText(list.get(position).getName()+"("+list.get(position).getSymbol()+")");
+        viewH.Valor.setText(list.get(position).getPrice_usd());
+        viewH.Volumen.setText(list.get(position).getPercent_change_24h());
+
+
+        return convertView;
+    }
+    private class ViewHolder {
+        ImageView imagen;
+        TextView Nombre;
+        TextView Valor;
+        TextView Volumen;
+    }
+}
--- app/src/main/java/es/com/lugman/appfinal2/MainActivity.java
+++ app/src/main/java/es/com/lugman/appfinal2/MainActivity.java
@@ -1,13 +1,117 @@
 package es.com.lugman.appfinal2;
 
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.ListView;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
-
+ListView list;
+    ArrayList<Monedas> listaMonedas;
+    Adaptador adp;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        TraerLista traer =  new TraerLista();
+        traer.execute();
+        list =  findViewById(R.id.listView);
+
+
+
     }
+    private class  TraerLista extends AsyncTask<String,String,Void>{
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+        }
+
+        @Override
+        protected void onProgressUpdate(String... values) {
+            super.onProgressUpdate(values);
+        }
+
+        @Override
+        protected void onPostExecute(Void aVoid) {
+            super.onPostExecute(aVoid);
+
+
+            Runnable  runa = new Runnable() {
+                @Override
+                public void run() {
+                    adp = new Adaptador(listaMonedas,MainActivity.this);
+                }
+            };
+            Thread  thead = new Thread(runa);
+            thead.run();
+              list.setAdapter(adp);
+        }
+
+        @Override
+        protected Void doInBackground(String... params) {
+            URL url = null;
+            HttpURLConnection urlConnection = null;
+            try {
+                url = new URL("http://lugman.com.es/dawanair/app/monenda_simple.php");
+                urlConnection = (HttpURLConnection) url.openConnection();
+                InputStream in = new BufferedInputStream(urlConnection.getInputStream());
+                BufferedReader reader =  new BufferedReader(new InputStreamReader(in));
+                String line;
+                while (((line = reader.readLine()) != null)){
+                    JSONArray arrJson = new JSONArray(line);
+                    listaMonedas = new ArrayList<Monedas>();
+
+                        for (int i=0;i <1;i++){
+                            Monedas moneda = new Monedas();
+                            JSONObject obj = (JSONObject) arrJson.get(i);
+                            Log.d("String ",obj.getString("rank"));
+
+                            moneda.setId(obj.getString("id"));
+                            moneda.setName(obj.getString("name"));
+                            moneda.setSymbol(obj.getString("symbol"));
+                            moneda.setRank(obj.getString("rank"));
+                            moneda.setPrice_btc(obj.getString("price_usd"));
+                            moneda.setPrice_usd(obj.getString("price_btc"));
+                            moneda.setVolume_usd_24(obj.getString("volume_usd_24"));
+                            moneda.setImage(obj.getString("image"));
+
+                            listaMonedas.add(moneda);
+                        }
+
+
+                }
+
+
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            } catch (JSONException e) {
+                e.printStackTrace();
+            } finally {
+                urlConnection.disconnect();
+
+            }
+            return null;
+        }
+
+
+    }
+
 }
