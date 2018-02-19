2d335d2e6a30e49ab444585bd3ba41957999bb81
Lugman <lugmansito@gmail.com>
2018-02-19 19:17:11 +0100

    FIN!2

--- app/src/main/java/es/com/lugman/appfinal2/MainActivity.java
+++ app/src/main/java/es/com/lugman/appfinal2/MainActivity.java
@@ -1,3 +1,11 @@
+/*
+ * Copyright (c) © 2018 Lugman Sami Ahmad Masnilla.
+ * El poseedor del Copyright tiene los siguientes derechos:
+ * DERECHOS DE AUTOR.
+ * Todos los contenidos de este Sitio (Incluyendo, pero no limitado a, texto, logotipos, contenido, fotografías, audio, botones, nombres comerciales y vídeo) están sujetos a derechos de propiedad por las leyes de Derechos de Autor y demás Leyes relativas Internacionales a Majo Mexa Producciones Ceamic i de terceros titulares de los mismos que han autorizado debidamente su inclusión.
+ *
+ */
+
 package es.com.lugman.appfinal2;
 
 import android.app.Dialog;
